54e7188f709b40fdfbaa62c2955bd23fdf03ee3c
Paweł <pawel.tymczuk@gmail.com>
2024-04-29 16:09:53 +0200

    chore: testing children prop

--- src/App.react.tsx
+++ src/App.react.tsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
-import { Counter } from "./sdk-react";
+import { useState } from 'react';
+import { Counter, CounterChild } from './sdk-react';
 
-import styles from "./App.module.css";
+import styles from './App.module.css';
+
+const Custom = () => {
+  console.log('Custom component rendered');
+  return <span>Hello from the Custom component</span>;
+};
 
 export function MyReactApp() {
   const [count, setCount] = useState(0);
@@ -13,7 +18,23 @@ export function MyReactApp() {
         The solid counter component will be rendered only when the host
         application counter module 3 equals to 0.
       </i>
-      {count % 3 !== 0 && <Counter initialCounter={count} />}
+      <br />
+      {count % 3 !== 0 && (
+        <div style={{ border: '1px solid cyan' }}>
+          Example: custom component as the prop
+          <Counter initialCounter={1}>
+            <Custom />
+          </Counter>
+        </div>
+      )}
+      {count % 3 !== 0 && (
+        <div style={{ border: '1px solid magenta' }}>
+          Example: Solid component wrapped in React component as the prop
+          <Counter initialCounter={1}>
+            <CounterChild />
+          </Counter>
+        </div>
+      )}
     </div>
   );
 }
--- src/sdk-react/components.react.tsx
+++ src/sdk-react/components.react.tsx
@@ -1,16 +1,28 @@
-import { useCallback } from "react";
-import { Portal } from "./portal.react";
-import { useMySDK } from "./hooks";
+import { useCallback } from 'react';
+import { Portal } from './portal.react';
+import { useMySDK } from './hooks';
 
 // Thin wrapper around the SDK's Counter component that provides a React-friendly API
-export function Counter(props: { initialCounter?: number }) {
+export function Counter(props: { children?: any; initialCounter?: number }) {
   const { mySdk } = useMySDK();
 
   const mount = useCallback(
     (node: HTMLDivElement) => mySdk.mountCounter(node, props),
-    []
+    [props]
   );
   const unmount = useCallback(() => mySdk.unmountCounter(), []);
+
+  return <Portal mount={mount} unmount={unmount} />;
+}
+
+export function CounterChild() {
+  const { mySdk } = useMySDK();
+
+  const mount = useCallback(
+    (node: HTMLDivElement) => mySdk.mountCounterChild(node),
+    []
+  );
+  const unmount = useCallback(() => mySdk.unmountCounterChild(), []);
 
   return <Portal mount={mount} unmount={unmount} />;
 }
